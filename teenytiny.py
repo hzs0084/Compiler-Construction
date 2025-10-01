dd59ff9adbe9fa1373d4e0153522a0e0e11b523c
Hemant Sherawat <hzs0084@kandah1.cse.eng.auburn.edu>
2025-10-01 13:36:00 -0500

    Finished building a parser following the blog

--- teenytiny.py
+++ teenytiny.py
@@ -1,12 +1,30 @@
 from lex import *
+from parse import *
+import sys
 
 def main():
-    source = "IF+-123 foo*THEN/"
-    lexer = Lexer(source)
+    print("Teeny Tiny Compiler")
 
-    token = lexer.getToken()
-    while token.kind != TokenType.EOF:
-        print(token.kind)
-        token = lexer.getToken()
+    # if len(sys.argv) != 2:
+    #         sys.exit("Error: Compiler needs source file as argument.")
+
+    # with open(sys.argv[1], "r") as inputFile:
+    # source = """ LABEL loop
+    #         PRINT "hello, world!"
+    #         GOTO loop""" #inputFile.read()
+
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    
+    with open(sys.argv[1], "r") as inputFile:
+        source = inputFile.read()
+
+    # Intialize the lexer anad parser
+    lexer = Lexer(source)
+    parser = Parser(lexer)
+
+    parser.program() # Start the parser
+
+    print("Parsing Completed")
 
 main()
